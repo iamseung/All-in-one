d4f13b23c33af5b354ab2ef0bd90c3d96e779775
iamseung <seesungseok@naver.com>
2024-10-15 23:44:48 +0900

    뼈대 추가 작업

---
+++ src/main/java/com/example/demo/service/EventListener.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component
+public class EventListener {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+}
